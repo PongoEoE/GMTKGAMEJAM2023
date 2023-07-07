34d232466722da98d60c1e5de975830a018e0822
PongoEoE <hudkolowrat@yahoo.com>
2023-07-07 14:04:49 -0500

    stuff

---
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+
+    //swimming
+    [HideInInspector]
+    public bool isSwimming;
+    public float swimSpeed;
+    void FixedUpdate()
+    {
+    
+    }
+    }
+    
+
---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+
+    private void  OnTriggerEnter(Collider other)
+    {
+       if(other.CompareTag("Player") && other.GetComponent<FirstPersonMovement>() != null) 
+       {
+        FirstPersonMovement movement = other.GetComponent<FirstPersonMovement>();
+       }
+    }
+
+    private void  OnTriggerExit(Collider other)
+    {
+        
+    }
+
+
+}
