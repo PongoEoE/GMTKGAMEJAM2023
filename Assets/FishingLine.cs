c845a7fe279bbd1b5d2122d219f146a06722381f
ApselFriedrich <47480949+ApselFriedrich@users.noreply.github.com>
2023-07-07 19:53:04 -0400

    line

---
+++ Assets/FishingLine.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform player;
+    private GameObject line;
+    void Start()
+    {
+        line = transform.GetChild(0).gameObject;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.position) < 5) {
+            line.SetActive(true);
+        } else {
+            line.SetActive(false);
+        }
+    }
+}
