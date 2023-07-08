a2b755512d3e6ff8d791f6f35789ffdcefd5514b
ApselFriedrich <47480949+ApselFriedrich@users.noreply.github.com>
2023-07-08 18:43:54 -0400

    fline

--- Assets/FishingLine.cs
+++ Assets/FishingLine.cs
@@ -7,10 +7,12 @@ public class FishingLine : MonoBehaviour
     // Start is called before the first frame update
     private Transform player;
     private GameObject line;
+    private LineRenderer fLine;
     void Start()
     {
         line = transform.GetChild(0).gameObject;
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        fLine = line.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,8 @@ public class FishingLine : MonoBehaviour
 
         if(Vector3.Distance(tpos, ppos) < 15f) {
             line.SetActive(true);
+            float lp = Mathf.InverseLerp(0, 15, Vector3.Distance(tpos, ppos));
+            fLine.widthMultiplier = Mathf.Lerp(0.07f, 0, lp);
         } else {
             line.SetActive(false);
         }
