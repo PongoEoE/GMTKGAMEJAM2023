739df8f82b1cebecbb27f386f52eb30e36c8406a
ApselFriedrich <47480949+ApselFriedrich@users.noreply.github.com>
2023-07-07 21:38:12 -0400

    UI

--- Assets/Scripts/Interaction System/Food.cs
+++ Assets/Scripts/Interaction System/Food.cs
@@ -9,7 +9,22 @@ public class Food : MonoBehaviour, IInteractable
     [SerializeField] private string prompt;
     public string InteractionPrompt => prompt;
 
+    [SerializeField] private RectTransform Ping;
+
     //[SerializeField] float currentHunger = player.getComponent<Hunger>().getHunger();
+    private Transform player;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void FixedUpdate() {
+        if(Vector3.Distance(transform.position, player.position) < 25) {
+            Ping.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+            Ping.gameObject.SetActive(true);
+        } else {
+            Ping.gameObject.SetActive(false);
+        }
+    }
 
     public bool Interact (Interactor interactor)
     {
