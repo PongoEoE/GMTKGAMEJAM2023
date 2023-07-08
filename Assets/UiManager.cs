e33638c236ab23cfc152030cfb14111254779f7b
ApselFriedrich <47480949+ApselFriedrich@users.noreply.github.com>
2023-07-07 21:07:38 -0400

    humgerbar

--- Assets/UiManager.cs
+++ Assets/UiManager.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] RawImage hungerUI;
+    [SerializeField] private RawImage hungerUI;
+    [SerializeField] private RawImage staminaUI;
+    private Hunger hunger;
     void Start()
     {
-
+        hunger = GameObject.FindGameObjectWithTag("Player").GetComponent<Hunger>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        SetHunger(hunger.getHunger());
     }
 
     public void SetHunger(float hunger) {
         hungerUI.rectTransform.sizeDelta = new Vector2((hunger/100) * 250f, 25f);
+    }
+
+    public void SetStamina(float stamina) {
+        staminaUI.rectTransform.sizeDelta = new Vector2((stamina/100) * 250f, 25f);
     }
 }
